9da8fe5ff611cddc2362d3c822b3e7695b9513a7
Matthew Yates <yatesm4@mail.lcc.edu>
2017-02-19 04:14:50 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+function overlayChange(element) {
+
+    var overlay = document.getElementById("welcomeOverlay");
+
+    var opac = 1.0;
+
+    var id = setInterval(frame, 10);
+
+
+    function frame() {
+
+        if (opac == 0.0) {
+            clearInterval(id);
+            overlay.style.display = "none";
+        } else {
+            opac -= 0.01;
+            overlay.style.opacity = opac;
+        }
+
+    }
+
+    var sidebar = document.getElementById("sidebarForm");
+
+    sidebar.style.display = "block";
+
+    codeAddress(element);
+}
 
